d781dadc614931be71d640a39223cb6185f4a9a2
Arpit <lengtharc@gmail.com>
2026-03-19 09:56:07 +0000

    CI added

--- test_sorting.py
+++ test_sorting.py
@@ -3,6 +3,6 @@ from bubble import bubblesort
 
 def test_basic():
     arr=[3,2,1]
-    assert bubblesort(arr())==[1,2,3]
+    assert bubblesort(arr.copy())==[1,2,3]
     assert sort(arr.copy())==[1,2,3]
 
