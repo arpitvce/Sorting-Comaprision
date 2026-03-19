5272ad2dd27d3557e04d8c9c2f086f8b2c7f78b8
Arpit <lengtharc@gmail.com>
2026-03-19 10:00:14 +0000

    CI added

--- bubble.py
+++ bubble.py
@@ -1,6 +1,6 @@
 def bubblesort(arr):
     for i in range(len(arr)):
-        for j in range(i,len(arr)-1):
+        for j in range(0,len(arr)-i-1):
             if arr[j]<arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
     return arr
